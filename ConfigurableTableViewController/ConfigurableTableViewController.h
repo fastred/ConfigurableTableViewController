77c3aedc5a38f7afdd7a5cce3554c307d9b9371c
Arkadiusz Holko <fastred@fastred.org>
2016-01-03 13:12:42 +0100

    Initial commit

---
+++ ConfigurableTableViewController/ConfigurableTableViewController.h
@@ -0,0 +1,19 @@
+//
+//  ConfigurableTableViewController.h
+//  ConfigurableTableViewController
+//
+//  Created by Arkadiusz Holko on 03-01-16.
+//  Copyright © 2016 Arkadiusz Holko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConfigurableTableViewController.
+FOUNDATION_EXPORT double ConfigurableTableViewControllerVersionNumber;
+
+//! Project version string for ConfigurableTableViewController.
+FOUNDATION_EXPORT const unsigned char ConfigurableTableViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConfigurableTableViewController/PublicHeader.h>
+
+
